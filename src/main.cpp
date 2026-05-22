32a343d07faa6912fffb13983c20eda36cb54598
Your Name <your-email@example.com>
2026-05-22 08:56:48 -0300

    só recebe espnow

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-uint8_t broadcastAddress[] = {0xEC, 0x64, 0xC9, 0x86, 0x1D, 0xE1};
-
 struct DHTData
 {
 	char temperature[65];
@@ -11,34 +9,10 @@ struct DHTData
 };
 DHTData dhtData;
 
-bool aceso = true;
-void piscar()
-{
-	aceso = !aceso;
-	digitalWrite(LED_BUILTIN, aceso);
-}
-
-void onDataSend(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-	Serial.print("Status do envio: ");
-
-	if (status == ESP_NOW_SEND_SUCCESS)
-	{
-		Serial.println("OK");
-	}
-	else
-	{
-		Serial.println("Falha no envio");
-	}
-}
 
 void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
 {
 	memcpy(&dhtData, incomingData, sizeof(dhtData));
-	if (sizeof(dhtData) > 0)
-	{
-		piscar();
-	}
 	Serial.println();
 	Serial.print("Bytes recebidos: ");
 	Serial.println(len);
@@ -46,20 +20,6 @@ void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
 	Serial.println(dhtData.temperature);
 	Serial.print("String: ");
 	Serial.println(dhtData.temperature);
-
-	esp_err_t result = esp_now_send(
-		 broadcastAddress,
-		 (uint8_t *)&dhtData,
-		 sizeof(dhtData));
-
-	if (result == ESP_OK)
-	{
-		Serial.println("Enviado com sucesso");
-	}
-	else
-	{
-		Serial.println("Erro ao enviar");
-	}
 }
 
 void setup()
@@ -81,20 +41,6 @@ void setup()
 	}
 
 	esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
-	//=============================================================
-	esp_now_register_send_cb(onDataSend);
-
-	esp_now_peer_info_t peerInfo = {};
-	memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-
-	peerInfo.channel = 0;
-	peerInfo.encrypt = false;
-
-	if (esp_now_add_peer(&peerInfo) != ESP_OK)
-	{
-		Serial.println("Erro ao adicionar peer");
-		return;
-	}
 }
 
 void loop()
